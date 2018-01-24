3d710bbbee1b9f3aa1420ef42a633f1cb6d35377
Brian Bixby <brianbixby0@gmail.com>
2018-01-23 17:32:46 -0800

    think this works

--- server.js
+++ server.js
@@ -1,81 +1,70 @@
 'use strict';
 
-//ALLOWS NODE TO INTERACT WITH LOCAL FILES CUS DB IS RUNNING LOCALLY
-const fs = require('fs');
-// GZIP COMPRESSION CAN GREATLY DECREASE THE SIZE OF THE RESPONSE BODY
-//const compression = require('compression')
+//ALLOWS EVERYONE TO VIEW
+const cors = require('cors');
 // ALLOWS SERVER SIDE JS TO BE EXECUTED
 const express = require('express');
-const app = express();
 // POSTGRES DBMS
 const pg = require('pg');
 // ALLOWS FOR PARSEING OF INCOMPING API POSTS AND PUTS, MANIPULATES REQ BODY BEFORE IT HITS SERVER
 const bodyParser = require('body-parser');
-// THE PORT WE RUN OUR SERVER ON
-const cors = require('cors');
-//ALLOWS EVERYONE TO VIEW
+const app = express();
 const PORT = process.env.PORT;
-// DATABASE PORT
+//ALLOWS NODE TO INTERACT WITH LOCAL FILES CUS DB IS RUNNING LOCALLY
+const fs = require('fs');
+
+
 const conString = process.env.DATABASE_URL;
-//const conString = 'postgres://localhost:5432/books_app';
-// FRONTEND URL
 //const CLIENT_URL = process.env.CLIENT_URL;
 const CLIENT_URL = 'https://taylor-brian-tama-bookapp.github.io/client';
 const client = new pg.Client(conString);
-
-
 // HOW WE CONNECT TO OUR DB
 client.connect();
 
 // ***  MIDDLEWARE  ***
-// TELLS EXPRESS TO USE GZIP COMPRESSION, REDUCES SIZE OF RES BODIES
-//app.use(compression())
+// CROSS ORIGIN SCRIPTING
+app.use(cors());
 // TELLS BODYPARSER TO USE JSON WHEN POSTING AND PUTING DATA INTO DB
 app.use(bodyParser.json());
 // ALLOWS BODYPARSER TO PARSE NESTED OBJECTS
 app.use(bodyParser.urlencoded({ extended: true }));
-// CROSS ORIGIN SCRIPTING
-app.use(cors());
-
 
 app.get('/', (req, res) => {
     res.send('hello');
 })
 // ROUTES 
-app.get('/v1/books', (req, res) => {
-    console.log('${conString}/v1/books');
-    client.query(`
-      SELECT * FROM books;
-      `
-    )
-    .then(result => res.send(result.rows))
-    .catch(console.error);
+app.get('/v1/books', function(req, res) {
+    console.log('app.get /v1/books');
+    client.query('SELECT * FROM books;')
+    .then(function(data) {
+      res.send(data.rows);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
   });
   
-  
-  app.post('/v1/books', (req, res) => {
-   
-    client.query(
-      `INSERT INTO
-      books (title, author, isbn, image_url, description)
-      VALUES ($1, $2, $3, $4, $5);
-      `,
-      [
-        request.body.title,
-        request.body.author,
-        request.body.isbn,
-        request.body.image_url,
-        request.body.description,
-        
-      ]
-    )
-      .then(function() {
-        response.send('insert complete')
-      })
-      .catch(function(err) {
-        console.error(err);
-      });
-  });
+app.post('/v1/books', function(req, res) {
+  client.query(
+    `INSERT INTO books (title, author, isbn, image_url, description)
+    VALUES ($1, $2, $3, $4, $5);
+    `,
+    [
+      req.body.title,
+      req.body.author,
+      req.body.isbn,
+      req.body.image_url,
+      req.body.description,
+      
+    ]
+  )
+    .then(function(data) {
+      res.send('insert complete');
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+});
     
 
 loadDB();
@@ -88,19 +77,17 @@ function loadBooks() {
     fs.readFile(`${CLIENT_URL}/data/books.json`, function(err, fd) {
       JSON.parse(fd.toString()).forEach(function(ele) {
         client.query(
-          'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;',
+          `INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;`,
           [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
         )
       })
     })
-   }
+   };
    
    function loadDB() {
     client.query(`
       CREATE TABLE IF NOT EXISTS
       books(id SERIAL PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), isbn VARCHAR(255), image_url VARCHAR(255), description TEXT NOT NULL);
       `)
-   
       .then(loadBooks());
-   }
-
+   };
